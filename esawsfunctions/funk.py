33dc6e1a0ead57a325be32f7298384939fc86ee6
thomashenson <thomasjameshenson@gmail.com>
2019-11-14 11:36:35 +0000

    Added movement calculations for imputation
    - Added two functions:
    	- movement_calculation_a
    	- movement_calculation_b
    - Both functions take current value of two iterable lists and performs equation, returning the result
    - Updated .gitignore to include .vscode

--- esawsfunctions/funk.py
+++ esawsfunctions/funk.py
@@ -263,3 +263,25 @@ def delete_data(bucket_name, file_name):
         return "Succesfully deleted file from S3 bucket."
     except ClientError:
         return "File does not exist in specified bucket!"
+
+
+def movement_calculation_a(current_value, previous_value):
+    """
+    Movements calculation for Sand and Gravel.
+    :param current_value: The current value for the current period - Type: Integer(?)
+    :param previous_value: The current value for the previous period - Type: Integer(?)
+    :return: Calculation value - Type: Integer(?)
+    """
+    number = (current_value - previous_value) / previous_value
+    return number
+
+
+def movement_calculation_b(current_value, previous_value):
+    """
+    Movements calculation for Bricks/Blocks.
+    :param current_value: The current value for the current period - Type: Integer(?)
+    :param previous_value: The current value for the previous period - Type: Integer(?)
+    :return: Calculation value - Type: Integer(?)
+    """
+    number = current_value / previous_value
+    return number
