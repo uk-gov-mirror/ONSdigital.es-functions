8559970e8eeabcb580d63ac8b4333356c5b850e7
Michael Candlin <mike@candlin.net>
2020-01-14 10:42:00 +0000

    Update aws_functions.py

--- es_aws_functions/aws_functions.py
+++ es_aws_functions/aws_functions.py
@@ -99,7 +99,7 @@ def get_dataframe(queue_url, bucket_name, key, incoming_message_group):
     (used to delete old message) - Type: String
     """
     data, receipt_handle = get_data(queue_url, bucket_name, key, incoming_message_group)
-    data = pd.read_json(data)
+    data = pd.read_json(data, dtype=False)
     return data, receipt_handle
 
 
