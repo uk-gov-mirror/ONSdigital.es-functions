8542d3e624f2c34886df5787e4cb6f1e6c1a1dde
Michael Candlin <michael@Michaels-MacBook-Pro.local>
2020-01-21 17:50:42 +0000

    i sort

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 setup(name='es_aws_functions',
       version='1.0',
       packages=find_packages(), install_requires=['boto3', 'botocore', 'pandas']
