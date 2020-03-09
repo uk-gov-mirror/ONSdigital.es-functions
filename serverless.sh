926209cfe06acf3a242d31d2dea6b04ad37b5cfe
Michael Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-09 16:41:20 +0000

    Update serverless.sh

--- serverless.sh
+++ serverless.sh
@@ -4,7 +4,7 @@
 cd functions-deploy-repository
 mkdir layer
 mkdir layer/python
-cp -R es_functions layer/python/es_functions
+cp -R es_aws_functions layer/python/es_aws_functions
 echo Deploying to AWS...
 serverless deploy --verbose;
 rm -rf layer/
