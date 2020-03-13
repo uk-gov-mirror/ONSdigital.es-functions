a0f25a0fbde00a056fdb4e3fd14ba18333911e3f
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-13 14:33:44 +0000

    reverting change

--- serverless.sh
+++ serverless.sh
@@ -5,16 +5,16 @@ cd functions-deploy-repository
 mkdir layer
 mkdir layer/python
 cp -R es_aws_functions layer/python/es_aws_functions
-mkdir pythonlayer
+#mkdir pythonlayer
 
-find / -name "site-packages"
-cp -r /usr/local/lib/python3.7/site-packages/ pythonlayer/site-packages
-ls
-cd pythonlayer
-ls
-mv site-packages/ python/
-zip python.zip python/
-cd ..
+
+#cp -r /usr/local/lib/python3.7/site-packages/ pythonlayer/site-packages
+
+#cd pythonlayer
+
+#mv site-packages/ python/
+#zip python.zip python/
+#cd ..
 
 echo Deploying to AWS...
 serverless deploy --verbose;
