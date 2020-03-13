8e04e649ac1038859641e2aebd4da8dfc8a5e709
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-13 13:51:30 +0000

    finding site-packages location

--- serverless.sh
+++ serverless.sh
@@ -9,7 +9,9 @@ cp -R es_aws_functions layer/python/es_aws_functions
 
 find / -name "site-packages"
 cp -r /usr/local/lib/python3.7/site-packages pythonlayer
+ls
 cd pythonlayer
+ls
 mv site-packages/ python/
 cd ..
 
