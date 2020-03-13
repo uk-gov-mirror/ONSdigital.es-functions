dc5ad7fc576be1ee59b98c2f19cc9e234fd1bfd0
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-13 10:30:31 +0000

    added pythonlayer deployment

--- serverless.sh
+++ serverless.sh
@@ -5,6 +5,13 @@ cd functions-deploy-repository
 mkdir layer
 mkdir layer/python
 cp -R es_aws_functions layer/python/es_aws_functions
+
+
+cp -r /usr/local/lib/python3.7/site-packages pythonlayer
+cd pythonlayer
+mv site-packages/ python/
+cd ..
+
 echo Deploying to AWS...
 serverless deploy --verbose;
 rm -rf layer/
