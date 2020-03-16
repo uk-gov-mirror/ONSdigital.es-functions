dae660daf650ce07cc7e461180ef6f712a354d0b
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-16 10:29:24 +0000

    100th times the charm

--- serverless.sh
+++ serverless.sh
@@ -10,12 +10,13 @@ mkdir pythonlayer
 ls /
 ls /site-packages
 #cp -r /usr/local/lib/python3.7/site-packages/ pythonlayer/site-packages
+cp -r /site-packages pythonlayer/site-packages
 
-#cd pythonlayer
+cd pythonlayer
 
-#mv site-packages/ python/
-#zip python.zip python/
-#cd ..
+mv site-packages/ python/
+zip python.zip python/
+cd ..
 
 echo Deploying to AWS...
 serverless deploy --verbose;
