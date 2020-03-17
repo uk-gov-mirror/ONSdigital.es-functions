39a4bd8ae72cfd23d7ca59e3ea9f6cb4b9535d42
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-03-17 12:47:27 +0000

    cleaned up script

--- serverless.sh
+++ serverless.sh
@@ -5,19 +5,11 @@ cd functions-deploy-repository
 mkdir layer
 mkdir layer/python
 cp -R es_aws_functions layer/python/es_aws_functions
+
 mkdir pythonlayer
-
-ls /
-ls /site-packages
-#cp -r /usr/local/lib/python3.7/site-packages/ pythonlayer/site-packages
 cp -r /site-packages pythonlayer/site-packages
-
 cd pythonlayer
-ls
-du site-packages
 mv site-packages/ python/
-du -c -h python
-ls
 zip python.zip python/
 cd ..
 
