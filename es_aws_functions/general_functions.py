f5596aeac250c734dd9dd45af9e89d50738411b8
Michael Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-12-17 12:23:44 +0000

    Update general_functions.py

--- es_aws_functions/general_functions.py
+++ es_aws_functions/general_functions.py
@@ -1,11 +1,10 @@
 import math
 import sys
 import traceback
-
 from es_aws_functions import aws_functions
-from spp_logger import SPPLogger, SPPLoggerConfig
 
 import immutables
+from spp_logger import SPPLogger, SPPLoggerConfig
 
 
 def calculate_adjacent_periods(current_period, periodicity):
