dc339959683a52fe097758295d2783ba1af067be
Luke Glanville <lukeglanville.ons@gmail.com>
2021-01-08 12:12:21 +0000

    Added dev_requirements into setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import find_packages, setup
 
+with open("./requirements.txt") as f:
+    dev_reqs = f.read()
+
 setup(name='es_aws_functions',
       version='1.0',
-      packages=find_packages(), install_requires=['boto3', 'botocore', 'pandas']
+      packages=find_packages(), install_requires=dev_reqs
       )
